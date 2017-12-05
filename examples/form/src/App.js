b95b78ae8a9f0bfdf2cb565a7c6463483ece959e
Christopher Aldama Perez <radiomistc@gmail.com>
2017-12-05 15:18:34 -0600

    Add Webpack config to guim

--- examples/form/src/App.js
+++ examples/form/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Checkbox } from 'guim';
+import Checkbox from 'guim';
+
+console.log('Checkbox', Checkbox)
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {checked: false};
+  }
+
+  onChange() {
+    this.setState({checked: !this.state.checked});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Checkbox name="checkbox" checked={this.state.checked} onChange={this.onChange.bind(this)} />
       </div>
     );
   }
