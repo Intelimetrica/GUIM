96abf48633d829816f0523eb084a4711c772ded1
fgarcia <fgarcia@intelimetrica.com>
2020-07-13 18:59:39 +0000

    Using if in row,colspan

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,22 +1,22 @@
-import React, { Component, Fragment} from "react";
+import React, { Component, Fragment } from "react";
 import "./styles.scss";
 
 export class StickyHeader extends Component {
   constructor(props) {
     super(props);
-    this.state = { active: false, width: '100%'};
+    this.state = { active: false, width: '100%' };
     this.handleScroll = this.handleScroll.bind(this);
   }
 
   handleScroll(e) {
     const originalHeaderTop = (this.originalHeader)
-      ?  this.originalHeader.getBoundingClientRect().top
+      ? this.originalHeader.getBoundingClientRect().top
       : Number.POSITIVE_INFINITY;
 
     if (originalHeaderTop <= this.props.top && !this.state.active) {
-      this.setState({active: true});
-    } else if (originalHeaderTop > this.props.top && this.state.active){
-      this.setState({active: false});
+      this.setState({ active: true });
+    } else if (originalHeaderTop > this.props.top && this.state.active) {
+      this.setState({ active: false });
     }
   }
 
@@ -50,7 +50,19 @@ export class StickyHeader extends Component {
           <thead>
             {headers.map((rowHeader, index_row) => (
               <tr id={head_id} className={`theader `} key={`header-${index_row}`}>
-                {rowHeader.map((colHeader, index_col) => <th className={colHeader.className} key={`row-header-${index_col}`} rowSpan={colHeader.rowSpan === undefined ? 1 :colHeader.rowSpan} colSpan={colHeader.colSpan === undefined ? 1 :colHeader.colSpan}>{colHeader.text}</th>)}
+                {rowHeader.map((colHeader, index_col) => {
+                  let options = {};
+                  if (colHeader.rowSpan) {
+                    options.rowSpan = colHeader.rowSpan;
+                  }
+                  if (colHeader.colSpan) {
+                    options.colSpan = colHeader.colSpan;
+                  }
+                  return (
+                    <th className={colHeader.className} key={`row-header-${index_col}`} {...options} >{colHeader.text}</th>)
+                }
+                )
+                }
               </tr>
             ))}
           </thead>
@@ -77,7 +89,17 @@ const Table = props => {
         <thead>
           {headers.map((rowHeader, index_row) => (
             <tr id={head_id} className={`theader `} key={`header-${index_row}`}>
-              {rowHeader.map((colHeader, index_col) => <th className={colHeader.className} key={`row-header-${index_col}`} rowSpan={colHeader.rowSpan === undefined ? 1 :colHeader.rowSpan} colSpan={colHeader.colSpan === undefined ? 1 :colHeader.colSpan}>{colHeader.text}</th>)}
+              {rowHeader.map((colHeader, index_col) => {
+                let options = {};
+                if (colHeader.rowSpan) {
+                  options.rowSpan = colHeader.rowSpan;
+                }
+                if (colHeader.colSpan) {
+                  options.colSpan = colHeader.colSpan;
+                }
+                return (
+                  <th className={colHeader.className} key={`row-header-${index_col}`} {...options} >{colHeader.text}</th>)
+              })}
             </tr>
           ))}
         </thead>
@@ -88,7 +110,7 @@ const Table = props => {
                 className={`trow ${(props.row_hovered === index) ? 'highlighted' : ''} ${props.row_className}`}
                 onMouseEnter={props.row_mouseEnter.bind(this, index)}
                 onMouseLeave={props.row_mouseLeave.bind(this, index)}>
-                {el.map((td, i) => <td key={`td-${index}-${i}`}>{td}</td> )}
+                {el.map((td, i) => <td key={`td-${index}-${i}`}>{td}</td>)}
               </tr>
             ))
           }
@@ -144,12 +166,12 @@ Table.defaultProps = {
     ]
   ],
   body: [
-    ["1", "John","Doe","john@doe.com", "View - Edit"],
-    ["2", "Jane","Doe","jane@doe.com", "View - Edit"],
-    ["3", "Josue","Doe","josue@doe.com", "View - Edit"]
+    ["1", "John", "Doe", "john@doe.com", "View - Edit"],
+    ["2", "Jane", "Doe", "jane@doe.com", "View - Edit"],
+    ["3", "Josue", "Doe", "josue@doe.com", "View - Edit"]
   ],
-  row_mouseEnter: (i) => {},
-  row_mouseLeave: (i) => {},
+  row_mouseEnter: (i) => { },
+  row_mouseLeave: (i) => { },
   className: "",
   row_className: "",
   row_hovered: -1, //  This one will set the given row's className to highlighted
