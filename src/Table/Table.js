7dc012d197aaba73e59d4f3932580be7c45fdb49
fgarcia <fgarcia@intelimetrica.com>
2020-07-13 18:44:54 +0000

    Noy undefined Col,rowspan

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -50,7 +50,7 @@ export class StickyHeader extends Component {
           <thead>
             {headers.map((rowHeader, index_row) => (
               <tr id={head_id} className={`theader `} key={`header-${index_row}`}>
-                {rowHeader.map((colHeader, index_col) => <th className={colHeader.className} key={`row-header-${index_col}`} rowSpan={colHeader.rowSpan} colSpan={colHeader.colSpan}>{colHeader.text}</th>)}
+                {rowHeader.map((colHeader, index_col) => <th className={colHeader.className} key={`row-header-${index_col}`} rowSpan={colHeader.rowSpan === undefined ? 1 :colHeader.rowSpan} colSpan={colHeader.colSpan === undefined ? 1 :colHeader.colSpan}>{colHeader.text}</th>)}
               </tr>
             ))}
           </thead>
