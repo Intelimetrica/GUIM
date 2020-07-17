25cadac4af99790ca8a592a85da8725dbdd9a538
fgarcia <fgarcia@intelimetrica.com>
2020-07-17 19:58:14 +0000

    Allow sorting by header

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useState } from "react";
+import React, { Component, Fragment } from "react";
 import "./styles.scss";
 
 export class StickyHeader extends Component {
